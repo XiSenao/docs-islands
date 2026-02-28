6b83ca8e4172242e1d89c75855c6dfbcdc0ffc5d
SenaoXi <email@senao.me>
2026-02-28 15:28:27 +0800

    refactor(utils): add recursive .env discovery and fix vite Plugin type compat

--- utils/index.ts
+++ utils/index.ts
@@ -8,4 +8,4 @@ export {
   type LogKind,
   type LogLevel,
 } from './logger';
-export { getProjectRoot, slash } from './path';
+export { findMonorepoRoot, getProjectRoot, isSubpath, slash } from './path';
