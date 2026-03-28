27904cf35b90129773113427bf4e6d87dcb302bc
SenaoXi <email@senao.me>
2026-03-28 12:28:57 +0800

    docs(vitepress): wire site debug console into docs

--- docs/.vitepress/theme/components/EnhanceLayout.vue
+++ docs/.vitepress/theme/components/EnhanceLayout.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
+import SiteDebugConsole from '@docs-islands/vitepress/debug-console';
 import { Analytics } from '@vercel/analytics/vue';
 import DefaultTheme from 'vitepress/theme';
 </script>
 
 <template>
-  <DefaultTheme.Layout>
-    <Analytics />
-  </DefaultTheme.Layout>
+  <DefaultTheme.Layout />
+  <Analytics />
+  <SiteDebugConsole />
 </template>
--- docs/client.d.ts
+++ docs/client.d.ts
@@ -2,3 +2,9 @@ declare module '*.svg' {
   const src: string;
   export default src;
 }
+
+declare module '@docs-islands/vitepress/debug-console' {
+  const SiteDebugConsole: any;
+
+  export default SiteDebugConsole;
+}
