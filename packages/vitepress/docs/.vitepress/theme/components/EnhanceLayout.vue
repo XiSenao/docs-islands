0fe542a87ae34e574ed14fe3b9e09597ac2dbd4e
SenaoXi <email@senao.me>
2026-04-29 23:33:02 +0800

    docs: add animated navbar logo

--- packages/vitepress/docs/.vitepress/theme/components/EnhanceLayout.vue
+++ packages/vitepress/docs/.vitepress/theme/components/EnhanceLayout.vue
@@ -6,7 +6,11 @@ import DefaultTheme from 'vitepress/theme';
 </script>
 
 <template>
-  <DefaultTheme.Layout />
+  <DefaultTheme.Layout v-bind="$attrs">
+    <template v-for="(_, name) in $slots" #[name]="slotProps">
+      <slot :name="name" v-bind="slotProps"></slot>
+    </template>
+  </DefaultTheme.Layout>
   <Analytics />
   <SiteDevToolsConsole />
 </template>
